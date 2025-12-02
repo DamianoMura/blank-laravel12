2dcd5e9d0cccb1f6276287699b07c409a8651c2f
DamianoMura <mura.damiano14@gmail.com>
2025-12-02 11:09:59 +0100

    [add] official blank project to use from now on....

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('content')
+    <div class="container mx-auto my-5 text-center">
+       
+        <h1 class="text-4xl font-bold mb-4">Welcome to JDWDEV:IT base Laravel project : {{config('app.name')}}</h1>
+        <p class="text-lg text-gray-700">This is the welcome page of your Laravel application.</p>
+        
+        jdwdev.it
+     
+@endsection 
